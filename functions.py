a29661b23fe9f86cdd8c52968e96a47cd09a3ef4
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2024-12-17 15:56:02 +0700

    update

--- functions.py
+++ functions.py
@@ -316,8 +316,8 @@ def get_driver():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
-    # service = service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-    service = Service()
+    service = service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    # service = Service()
     return webdriver.Chrome(
         service=service,
         options=options,
